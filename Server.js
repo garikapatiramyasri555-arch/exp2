ee5a3cfbc6e157c4aa9c2402cb9af3b0fab5de22
garikapatiramyasri555-arch <garikapatiramyasri555@gmail.com>
2026-06-29 21:08:54 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,16 +1,14 @@
-
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const userRoutes    = require('./routes/users');
-const productRoutes = require('./routes/products');
-
-app.use('/users',    userRoutes);     // Mount users router
-app.use('/products', productRoutes);  // Mount products router
+// test route
+app.get('/', (req, res) => {
+  res.send('Server is working 🚀');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
